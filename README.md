a627e4321b1766e9185a2a8ada2fec24cf012570
jaykakadiya18 <jaykakadiya2014@gmail.com>
2024-06-04 15:59:42 +0530

    init code

---
+++ README.md
@@ -0,0 +1,68 @@
+![spoa](https://github.com/Kichi779/Spotify-Streaming-Bot/assets/92625816/3899d0b7-37e9-4fce-a3ab-5d757182157f)
+ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
+
+### Please leave a star for the project. This project is free and open source. ⭐ You can leave a star from the top right. ⭐⭐
+
+This project is a Spotify listener bot. With this bot, it automatically logs into Spotify web and then opens the Spotify song link it asks you for, pressing the play button. The foundation of the project is Python Selenium.
+
+The project reflects as a listener to you. The project is new and will be supported with updates, but my sole motivation is for you to give the project a star.
+
+The program is new, still in development, please report any bugs to us on the discord server. ⚠️
+
+# Preview 💬
+ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤAt the end of the video are spotify artist results.
+
+https://github.com/Kichi779/Spotify-Streaming-Bot/assets/92625816/3e0180de-181b-492c-ac26-932c624e4e93
+
+# 📥 Installation & Usage
+
+### - For the detailed installation pdf, which is the solution to all errors, come to my [discord server](https://discord.gg/AFV9m8UXuT)
+
+- Python must be installed
+- If your Python is not installed, download the latest version (3.11) of python and make sure to click on the 'ADD TO PATH' during installation.
+
+- ### Install the chromedriver version compatible with the installed version of Google Chrome on your computer. ⚠️
+
+  https://sites.google.com/chromium.org/driver/?pli=1
+  
+```sh
+git clone https://github.com/Kichi779/Spotify-Streaming-Bot/ kichispotify
+
+cd kichispotify
+
+pip install -r requirements.txt (Or you can click on install.bat file)
+```
+## Usage ⚙️
+
+- **After completing the installation process, make sure that you have successfully installed the chromedriver.**
+
+- Type mail:password line by line in the accounts.txt file.
+
+- **Then run the run.bat file or double click the main.py file.**
+
+- **If you are going to use a proxy, write n if not using y.**
+
+- **Enter the Spotify song URL**
+
+- **My advice to you is not to use the keyboard and mouse before all processes are finished. In some places, the program uses the escape key.**
+
+
+# Usage recommendations 📖
+
+- This system opens multiple background sites, so make sure you have plenty of RAM or perform this operation on an unused computer. (It could be on a powerful virtual server.)
+
+- Premium accounts give clearer results, new accounts may not generate income for you.
+
+-I do not recommend changing the time sleep settings in the program, it is guaranteed to be slow, I tried to make it behave like a real person
+
+# Stars ⭐
+- Proxy system. Each account is connected with a proxy / 150 Star ⭐ ❌
+
+
+# ⚠️Legal⚠️
+
+The software designed to perform website security testing. The author is not responsible for any illegal use of these programs. This is 100% educational, please do not misuse this tool. 
+
+If there is a legal problem, please contact my e-mail address. 
+
+kichi779@protonmail.com
---
+++ spotifystreambot.py
@@ -0,0 +1,252 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+from webdriver_manager.chrome import ChromeDriverManager
+import requests
+import webbrowser
+import random
+import pytz
+import time
+import os
+import keyboard
+from colorama import Fore
+from pystyle import Center, Colors, Colorate
+import time
+
+os.system(f"title Kichi779 - Spotify Streaming bot v1 ")
+
+url = "https://github.com/Kichi779/Spotify-Streaming-Bot/"
+
+def check_for_updates():
+    try:
+        r = requests.get("https://raw.githubusercontent.com/Kichi779/Spotify-Streaming-Bot/main/version.txt")
+        remote_version = r.content.decode('utf-8').strip()
+        local_version = open('version.txt', 'r').read().strip()
+        if remote_version != local_version:
+            print(Colors.red, Center.XCenter("A new version is available. Please download the latest version from GitHub"))
+            time.sleep(2)
+            webbrowser.open(url)
+            return False
+        return True
+    except:
+        return True
+
+def print_announcement():
+    try:
+        r = requests.get("https://raw.githubusercontent.com/Kichi779/Spotify-Streaming-Bot/main/announcement.txt", headers={"Cache-Control": "no-cache"})
+        announcement = r.content.decode('utf-8').strip()
+        return announcement
+    except:
+        print("Could not retrieve announcement from GitHub.\n")
+
+supported_timezones = pytz.all_timezones
+
+def set_random_timezone(driver):
+    random_timezone = random.choice(supported_timezones)
+    driver.execute_cdp_cmd("Emulation.setTimezoneOverride", {"timezoneId": random_timezone})
+
+def set_fake_geolocation(driver, latitude, longitude):
+    params = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "accuracy": 100
+    }
+    driver.execute_cdp_cmd("Emulation.setGeolocationOverride", params)
+
+def main():
+    if not check_for_updates():
+        return
+    announcement = print_announcement()
+    print(Colorate.Vertical(Colors.white_to_green, Center.XCenter("""
+           
+                       ▄█   ▄█▄  ▄█    ▄████████    ▄█    █▄     ▄█  
+                       ███ ▄███▀ ███   ███    ███   ███    ███   ███  
+                       ███▐██▀   ███▌  ███    █▀    ███    ███   ███▌ 
+                      ▄█████▀    ███▌  ███         ▄███▄▄▄▄███▄▄ ███▌ 
+                     ▀▀█████▄    ███▌  ███        ▀▀███▀▀▀▀███▀  ███▌ 
+                       ███▐██▄   ███   ███    █▄    ███    ███   ███  
+                       ███ ▀███▄ ███   ███    ███   ███    ███   ███  
+                       ███   ▀█▀ █▀    ████████▀    ███    █▀    █▀   
+                       ▀                                             
+ Improvements can be made to the code. If you're getting an error, visit my discord.  
+                             Github  github.com/kichi779    """)))
+    print("")
+    print(Colors.red, Center.XCenter("ANNOUNCEMENT"))
+    print(Colors.yellow, Center.XCenter(f"{announcement}"))
+    print("")
+
+    user_agents = [
+    # Chrome (Windows)
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36",
+
+    # Chrome (Mac)
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_4_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36",
+
+    # Firefox (Windows)
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0",
+    "Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0",
+
+    # Firefox (Mac)
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:93.0) Gecko/20100101 Firefox/93.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 11.4; rv:93.0) Gecko/20100101 Firefox/93.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:93.0) Gecko/20100101 Firefox/93.0",
+
+    # Safari (Mac)
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_4_0) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15",
+
+    # Opera (Windows)
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 OPR/80.0.4170.61",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 OPR/80.0.4170.61",
+    "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 OPR/80.0.4170.61"
+    ]
+    
+    #FAKE Language
+    supported_languages = [
+    "en-US", "en-GB", "en-CA", "en-AU", "en-NZ", "fr-FR", "fr-CA", "fr-BE", "fr-CH", "fr-LU",
+    "de-DE", "de-AT", "de-CH", "de-LU", "es-ES", "es-MX", "es-AR", "es-CL", "es-CO", "es-PE",
+    "it-IT", "it-CH", "ja-JP", "ko-KR", "pt-BR", "pt-PT", "ru-RU", "tr-TR", "nl-NL", "nl-BE",
+    "sv-SE", "da-DK", "no-NO"
+    ]
+
+    chrome_path = 'C:\Program Files\Google\Chrome\Application\chrome.exe'
+    driver_path = 'chromedriver.exe'
+
+    random_user_agent = random.choice(user_agents)
+
+    with open('accounts.txt', 'r') as file:
+        accounts = file.readlines()
+
+    proxies = []
+
+    use_proxy = input(Colorate.Vertical(Colors.green_to_blue, "Do you want to use proxies? (y/n):"))
+
+    if use_proxy.lower() == 'y':
+        print(Colors.red, Center.XCenter("The proxy system will be added after 50 stars. I continue to process without a proxy"))
+        with open('proxy.txt', 'r') as file:
+            proxies = file.readlines()
+        time.sleep(3)
+
+    spotify_song = input(Colorate.Vertical(Colors.green_to_blue, "Enter the Spotify song URL (e.g https://open.spotify.com/track/5hFkGfx038V0LhqI0Uff2J?si=bf290dcc9a994c36):"))
+
+    drivers = []
+
+    delay = random.uniform(2, 6)
+    delay2 = random.uniform(5, 14)
+
+    for account in accounts:
+
+        random_language = random.choice(supported_languages)
+
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_experimental_option('excludeSwitches', ["enable-automation", 'enable-logging'])
+        chrome_options.add_argument('--disable-logging')
+        chrome_options.add_argument('--log-level=3')
+        chrome_options.add_argument('--disable-infobars')
+        chrome_options.add_argument('--disable-extensions')
+        chrome_options.add_argument("--window-size=1366,768")
+        chrome_options.add_argument("--lang=en-US,en;q=0.9")
+        chrome_options.add_argument("--disable-notifications")
+        chrome_options.add_argument(f"--user-agent={random_user_agent}")
+        chrome_options.add_argument(f"--lang={random_language}")
+        chrome_options.add_argument("--mute-audio")
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_experimental_option('prefs', {
+            'profile.default_content_setting_values.notifications': 2
+        })
+
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+        username, password = account.strip().split(':')
+
+        try:
+            driver.get("https://www.spotify.com/us/login/")
+
+            username_input = driver.find_element(By.CSS_SELECTOR, "input#login-username")
+            password_input = driver.find_element(By.CSS_SELECTOR, "input#login-password")
+
+            username_input.send_keys(username)
+            password_input.send_keys(password)
+
+            driver.find_element(By.CSS_SELECTOR, "button[data-testid='login-button']").click()
+
+            time.sleep(delay)
+
+            driver.get(spotify_song)
+
+            driver.maximize_window()
+
+            keyboard.press_and_release('esc')
+
+            time.sleep(10)
+
+            try:
+                cookie = driver.find_element(By.XPATH, "//button[text()='Accept Cookies']")
+                cookie.click()
+            except NoSuchElementException:
+                try:
+                    button = driver.find_element(By.XPATH, "//button[contains(@class,'onetrust-close-btn-handler onetrust-close-btn-ui')]")
+                    button.click()
+                except NoSuchElementException:
+                    time.sleep(delay2)
+
+            playmusic_xpath = "(//button[@data-testid='play-button']//span)[3]"
+            playmusic = driver.find_element(By.XPATH, playmusic_xpath)
+            playmusic.click()
+
+            time.sleep(1)
+
+            print(Colors.green, "Username: {} - Listening process has started.".format(username))
+
+        except Exception as e:
+            print(Colors.red, "An error occurred in the bot system:", str(e))
+
+        set_random_timezone(driver)
+        
+        # FAKE LOCATION
+        latitude = random.uniform(-90, 90)
+        longitude = random.uniform(-180, 180)
+        set_fake_geolocation(driver, latitude, longitude)
+
+        drivers.append(driver)
+
+        time.sleep(5)
+
+    print(Colors.blue, "Stream operations are completed. You can stop all transactions by closing the program.")
+
+    while True:
+        pass
+
+if __name__ == "__main__":
+    main()
+
+
+# ==========================================
+# Copyright 2023 Kichi779
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==========================================
